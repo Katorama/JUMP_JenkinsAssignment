407d7ea117ca7d03fa9fde34d095d157d57232c2
Katorama <79383140+Katorama@users.noreply.github.com>
2021-10-19 15:07:18 -0400

    Add files via upload

---
+++ jenkinsAssignment.java
@@ -0,0 +1,10 @@
+public class jenkinsAssignment{
+
+    public static void main(String args[]){
+        int x = 1;
+        if(x <= 10) {
+            System.out.println(x);
+            x = x+1;
+        }
+    }
+}
